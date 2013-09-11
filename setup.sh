8f7f8af616895fea9c3a74b76331505475037f74
Artem Pyanykh <artem.pyanykh@gmail.com>
2013-09-11 19:35:46 +0400

    Init

---
+++ setup.sh
@@ -0,0 +1,6 @@
+DOTFILESDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Creating symbolic link to dotfiles"
+
+rm -rf "$HOME/.config/sublime-text-2/Packages/User"
+ln -sf "$DOTFILESDIR/User" "$HOME/.config/sublime-text-2/Packages/User"
