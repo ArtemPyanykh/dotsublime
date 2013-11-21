d4748ab991314e1866373555eb43b02cee74159e
Artem Pyanykh <artem.pyanykh@gmail.com>
2013-11-22 01:01:40 +0400

    Improve sublime setup, switch to Mac OS X

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 DOTFILESDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SUBLIME_USER_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
 echo "Creating symbolic link to dotfiles"
 
-rm -rf "$HOME/.config/sublime-text-3/Packages/User"
-ln -sf "$DOTFILESDIR/User" "$HOME/.config/sublime-text-3/Packages/User"
+if [ -d SUBLIME_USER_DIR ]; then
+  rm -rf SUBLIME_USER_DIR
+fi
+
+ln -sfv "$DOTFILESDIR/User" "$SUBLIME_USER_DIR"
