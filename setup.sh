6e1d4374cae0e036a81db9589e89d01ab74ea513
Artem Pyanykh <artem.pyanykh@gmail.com>
2013-11-23 11:49:14 +0400

    Fix setup

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ SUBLIME_USER_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User
 
 echo "Creating symbolic link to dotfiles"
 
-if [ -d SUBLIME_USER_DIR ]; then
-  rm -rf SUBLIME_USER_DIR
+if [ -d "$SUBLIME_USER_DIR" ]; then
+  echo "Removing $SUBLIME_USER_DIR"
+  rm -rf "$SUBLIME_USER_DIR"
 fi
 
 ln -sfv "$DOTFILESDIR/User" "$SUBLIME_USER_DIR"
